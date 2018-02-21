1b4a90381596d935dc8bc2dd6a872b70d1743a89
jefelewis <jeffylew77@gmail.com>
2018-02-20 16:44:08 -0800

    ProfileInfo & ProfilePic skeleton complete

---
+++ client/public/components/profilebar/ProfileInfo.jsx
@@ -0,0 +1,23 @@
+// Requirements: React
+import React from 'react';
+
+
+// Component: ProfileInfo
+class ProfileInfo extends React.Component{
+  constructor(){
+    super();
+  }
+
+
+  render() {
+    return (
+      <div>
+        
+
+
+      </div>
+    )
+  }
+
+
+}
---
+++ client/public/components/profilebar/ProfilePic.jsx
@@ -0,0 +1,19 @@
+// Requirements: React
+import React from 'react';
+
+
+// Component: ProfilePic
+class Profile extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        
+
+      </div>
+    )
+  }
+}
