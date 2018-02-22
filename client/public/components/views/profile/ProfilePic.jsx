46b64a5c86cdb33de2aded0bff1cbbba02e5d105
Jake <jmleavell90@gmail.com>
2018-02-21 16:27:14 -0800

    Profile Fix

---
+++ client/public/components/views/profile/ProfilePic.jsx
@@ -0,0 +1,20 @@
+// Requirements: React
+import React from 'react';
+
+
+// Component: ProfilePic
+class ProfilePic extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div id="profilepic">
+        <img src=""></img>
+      </div>
+    )
+  }
+}
+
+export default ProfilePic;
