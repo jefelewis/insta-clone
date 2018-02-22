256afb82abd2c7afb060c88020574380150eefbf
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-22 15:46:29 -0800

    style updates

--- client/public/App.jsx
+++ client/public/App.jsx
@@ -70,14 +70,14 @@ class App extends Component {
                 <Banner />
                 <ul>
                     <li>
-                    <Link to="/">Home</Link>
-                    <Link to="/user">User</Link>
-                    <Link to="/login">Login</Link>
+                    <Link to="/"> Home </Link>
+                    <Link to="/user"> User </Link>
+                    <Link to="/login"> Login </Link>
                     </li>
                 </ul>
-                <Route exact path="/" component={View} />
                 <Route path="/user" component={Profile} />
                 <Route path="/login" render={(props) => ( <Login change={this.onChangeHandler} click={this.onClickHandler}/> )} />
+                <Route exact path="/" component={View} />
                 {/* <Login change={this.onChangeHandler} click={this.onClickHandler} /> */}
                 <View />
                 </div>
@@ -85,5 +85,5 @@ class App extends Component {
         );
     }
 }
-
+//on banner component, in logout button set a state to banner component that lets it toggle it on or off
 export default App;
--- client/public/components/Banner.jsx
+++ client/public/components/Banner.jsx
@@ -13,8 +13,8 @@ class Banner extends Component {
           <form className="navbar-form navbar-left" role="search">
           <div className="form-group">
           <input type="text" className="form-control" placeholder="Search"/>
-          </div>
           <button type="submit" className="btn btn-default">Submit</button>
+          </div>
           </form>
         );
     }
