98de80cc7351ef02cb40aec44c342964906e4f73
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-23 08:28:26 -0800

    working dropdown button with minor bug

--- client/public/components/Banner.jsx
+++ client/public/components/Banner.jsx
@@ -1,69 +1,23 @@
-import React, { Component } from "react";
-import Search from "./banner/Search.jsx";
-import Details from "./banner/Details.jsx";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import Search from './banner/Search.jsx';
+import Details from './banner/Details.jsx';
+
 
 class Banner extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor() {
+        super();
+    }
 
-  render() {
-    return (
-      <div className="btn-group">
-
-
-      <div className="col-md-4">
-        <div className="left">
-          <img src="../TopShelf.png" height="100" width="100" />
-        </div>
-        </div>
-
-
-
-      <div className="col-md-4">
-        <div className="top">
-        <form className="navbar-form navbar-left" role="search">
+    render() {
+        return (
+          <form className="navbar-form navbar-left" role="search">
           <div className="form-group">
-            <input type="text" placeholder="Search" />
-            <button type="submit" className="btn btn-default">
-              Submit
-            </button>
+          <input type="text" className="form-control" placeholder="Search"/>
+          <button type="submit" className="btn btn-default">Submit</button>
           </div>
-        </form>
-        </div>
-        </div>
-
-
-        <div className="col-md-4">
-        <div className="right">
-        <button
-          type="button"
-          className="btn btn-default dropdown-toggle"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          float="right">
-          <img src="../ButtonPic.jpg" height="100" width="100" />
-          <span className="caret" />
-        <ul className="dropdown-menu">
-          <li>
-            <Link to="/"> Home </Link>
-          </li>
-          <li>
-            <Link to="/user"> User </Link>
-          </li>
-          <li role="separator" className="divider" />
-          <li>
-            <Link to="/login"> Login </Link>
-          </li>
-        </ul>
-        </button>
-        </div>
-        </div>
-      </div>
-    );
-  }
+          </form>
+        );
+    }
 }
 
 export default Banner;
