8217e602b68e818505c1aa07eb5c86f40de3d68a
Jake <jmleavell90@gmail.com>
2018-02-23 15:07:09 -0800

    Fixed Conflicts

--- client/public/App.jsx
+++ client/public/App.jsx
@@ -8,13 +8,14 @@ import firebase from 'firebase';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onClickHandler = this.onClickHandler.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
 
   componentWillMount() {
     var config = {
@@ -28,7 +29,12 @@ class App extends Component {
 
     firebase.initializeApp(config);
 
+<<<<<<< f7ccab24bb13ad34990aa91197468eaffbe73a4b
     firebase.auth().onAuthStateChanged((User) => {
+=======
+<<<<<<< 525ff89b7ab80ac0d7cdf114d7171d39ae1f8b00
+    firebase.auth().onAuthStateChanged(User => {
+>>>>>>> Fixed Conflicts
       if (User) {
         console.log(User, 'logged in!');
       } else {
@@ -53,7 +59,44 @@ class App extends Component {
       const errHandler = firebase
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password);
+<<<<<<< f7ccab24bb13ad34990aa91197468eaffbe73a4b
       errHandler.catch((e) => console.log(e.message));
+=======
+      errHandler.catch(e => console.log(e.message));
+=======
+        firebase.auth().onAuthStateChanged((User) => {
+            if (User) {
+                console.log(User, 'logged in!');
+            } else {
+                console.log('Logged out!')
+            }
+        })
+    }
+    onChangeHandler(e) {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+        console.log(this.state);
+    }
+    onClickHandler(e) {
+        if (e.target.name === 'signin') {
+            const errHandler = firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+            errHandler.catch(e => console.log(e.message));
+        } else {
+            const errHandler = firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+            errHandler.catch(e => console.log(e.message));
+        }
+        
+    }
+    render() {
+        return (
+					<div>
+					<Banner />
+					<View click={this.onClickHandler} change={this.onChangeHandler}/>
+					</div>
+        );
+>>>>>>> Cleaned Up App.jsx
+>>>>>>> Fixed Conflicts
     }
   }
   render() {
