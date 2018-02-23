1f19152a2ee861ef0ceafdf37a213e26c99049fe
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-22 17:54:01 -0800

    styling for navbar done, route functionality broken

--- client/public/App.jsx
+++ client/public/App.jsx
@@ -1,83 +1,90 @@
-import React, { Component } from 'react';
-import Banner from './components/Banner.jsx';
-import View from './components/View.jsx';
-import API from './config.js';
-import Login from './components/views/Login.jsx';
-import Profile from './components/views/Profile.jsx';
-const firebase = require('firebase');
+import React, { Component } from "react";
+import Banner from "./components/Banner.jsx";
+import View from "./components/View.jsx";
+import API from "./config.js";
+import Login from "./components/views/Login.jsx";
+import Profile from "./components/views/Profile.jsx";
+const firebase = require("firebase");
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            
-        }
-        // this.componentWillMount = this.componentWillMount.bind(this);
-        this.onChangeHandler = this.onChangeHandler.bind(this);
-        this.onClickHandler = this.onClickHandler.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {};
+    // this.componentWillMount = this.componentWillMount.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
 
-    componentWillMount() {
-        var config = {
-            apiKey: API.fireBaseApiKey,
-            authDomain: "top-shelf-708be.firebaseapp.com",
-            databaseURL: "https://top-shelf-708be.firebaseio.com",
-            projectId: "top-shelf-708be",
-            storageBucket: "top-shelf-708be.appspot.com",
-            messagingSenderId: "1039726774762"
-            };
-        firebase.initializeApp(config);
+  componentWillMount() {
+    var config = {
+      apiKey: API.fireBaseApiKey,
+      authDomain: "top-shelf-708be.firebaseapp.com",
+      databaseURL: "https://top-shelf-708be.firebaseio.com",
+      projectId: "top-shelf-708be",
+      storageBucket: "top-shelf-708be.appspot.com",
+      messagingSenderId: "1039726774762"
+    };
+    firebase.initializeApp(config);
 
-        firebase.auth().onAuthStateChanged((User) => {
-            if (User) {
-                console.log(User, 'logged in!');
-            } else {
-                console.log('Logged out!')
-            }
-        })
+    firebase.auth().onAuthStateChanged(User => {
+      if (User) {
+        console.log(User, "logged in!");
+      } else {
+        console.log("Logged out!");
+      }
+    });
+  }
+  onChangeHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state);
+  }
+  onClickHandler(e) {
+    //TODO: CHECK IF VALID EMAIL
+    if (e.target.name === "signin") {
+      const errHandler = firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password);
+      errHandler.catch(e => console.log(e.message));
+    } else {
+      const errHandler = firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password);
+      errHandler.catch(e => console.log(e.message));
     }
-    onChangeHandler(e) {
-        this.setState({
-          [e.target.name]: e.target.value
-        });
-        console.log(this.state);
-    }
-    onClickHandler(e) {
-        //TODO: CHECK IF VALID EMAIL
-        if (e.target.name === 'signin') {
-            const errHandler = firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
-            errHandler.catch(e => console.log(e.message));
-        } else {
-            const errHandler = firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
-            errHandler.catch(e => console.log(e.message));
-        }
-        
-    }
-    render() {
-        
-        return (
-            // <div>
-            //   {/*Load Login if not logged in  */}
-            //   <Login change={this.onChangeHandler} click={this.onClickHandler} />
-            //   {/* Otherwise Load Banner and View */}
-            //   <Banner />
-            //   <View />
-            // </div>
-            <Router>
-                <div>
-                <Banner />
-                
-                <Route path="/user" component={Profile} />
-                <Route path="/login" render={(props) => ( <Login change={this.onChangeHandler} click={this.onClickHandler}/> )} />
-                <Route exact path="/" component={View} />
-                {/* <Login change={this.onChangeHandler} click={this.onClickHandler} /> */}
-                <View />
-                </div>
-            </Router>
-        );
-    }
+  }
+  render() {
+    return (
+      // <div>
+      //   {/*Load Login if not logged in  */}
+      //   <Login change={this.onChangeHandler} click={this.onClickHandler} />
+      //   {/* Otherwise Load Banner and View */}
+      //   <Banner />
+      //   <View />
+      // </div>
+      <Router>
+        <div>
+          <Banner />
+
+          <Route path="/user" component={Profile} />
+          <Route
+            path="/login"
+            render={props => (
+              <Login
+                change={this.onChangeHandler}
+                click={this.onClickHandler}
+              />
+            )}
+          />
+          <Route exact path="/" component={View} />
+          {/* <Login change={this.onChangeHandler} click={this.onClickHandler} /> */}
+          <View />
+        </div>
+      </Router>
+    );
+  }
 }
 //on banner component, in logout button set a state to banner component that lets it toggle it on or off
 export default App;
--- client/public/components/Banner.jsx
+++ client/public/components/Banner.jsx
@@ -2,28 +2,50 @@ import React, { Component } from "react";
 import Search from "./banner/Search.jsx";
 import Details from "./banner/Details.jsx";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// jquery bootstrap ajax
-const $ = require('jQuery');
-const bootstrap = require('bootstrap');
 
 class Banner extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <div className="btn-group">
+
+
+      <div className="col-md-4">
+        <div className="left">
+          <img src="../TopShelf.png" height="100" width="100" />
+        </div>
+        </div>
+
+
+
+      <div className="col-md-4">
+        <div className="top">
+        <form className="navbar-form navbar-left" role="search">
+          <div className="form-group">
+            <input type="text" placeholder="Search" />
+            <button type="submit" className="btn btn-default">
+              Submit
+            </button>
+          </div>
+        </form>
+        </div>
+        </div>
+
+
+        <div className="col-md-4">
+        <div className="right">
         <button
           type="button"
           className="btn btn-default dropdown-toggle"
           data-toggle="dropdown"
           aria-haspopup="true"
           aria-expanded="false"
-        >
-          Profilepic
+          float="right">
+          <img src="../ButtonPic.jpg" height="100" width="100" />
           <span className="caret" />
-        </button>
         <ul className="dropdown-menu">
           <li>
             <Link to="/"> Home </Link>
@@ -36,14 +58,9 @@ class Banner extends Component {
             <Link to="/login"> Login </Link>
           </li>
         </ul>
-        <form className="navbar-form navbar-left" role="search">
-          <div className="form-group">
-            <input type="text" className="form-control" placeholder="Search" />
-            <button type="submit" className="btn btn-default">
-              Submit
-            </button>
-          </div>
-        </form>
+        </button>
+        </div>
+        </div>
       </div>
     );
   }
