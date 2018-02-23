c6b31820e8aa7953b0cdca02f5b26cc9e129bb80
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-22 16:55:02 -0800

    navbar v1 implemented

--- client/public/App.jsx
+++ client/public/App.jsx
@@ -68,13 +68,7 @@ class App extends Component {
             <Router>
                 <div>
                 <Banner />
-                <ul>
-                    <li>
-                    <Link to="/"> Home </Link>
-                    <Link to="/user"> User </Link>
-                    <Link to="/login"> Login </Link>
-                    </li>
-                </ul>
+                
                 <Route path="/user" component={Profile} />
                 <Route path="/login" render={(props) => ( <Login change={this.onChangeHandler} click={this.onClickHandler}/> )} />
                 <Route exact path="/" component={View} />
--- client/public/components/Banner.jsx
+++ client/public/components/Banner.jsx
@@ -1,23 +1,52 @@
-import React, { Component } from 'react';
-import Search from './banner/Search.jsx';
-import Details from './banner/Details.jsx';
-
+import React, { Component } from "react";
+import Search from "./banner/Search.jsx";
+import Details from "./banner/Details.jsx";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// jquery bootstrap ajax
+const $ = require('jQuery');
+const bootstrap = require('bootstrap');
 
 class Banner extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-          <form className="navbar-form navbar-left" role="search">
+  render() {
+    return (
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn btn-default dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Profilepic
+          <span className="caret" />
+        </button>
+        <ul className="dropdown-menu">
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/user"> User </Link>
+          </li>
+          <li role="separator" className="divider" />
+          <li>
+            <Link to="/login"> Login </Link>
+          </li>
+        </ul>
+        <form className="navbar-form navbar-left" role="search">
           <div className="form-group">
-          <input type="text" className="form-control" placeholder="Search"/>
-          <button type="submit" className="btn btn-default">Submit</button>
+            <input type="text" className="form-control" placeholder="Search" />
+            <button type="submit" className="btn btn-default">
+              Submit
+            </button>
           </div>
-          </form>
-        );
-    }
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Banner;
