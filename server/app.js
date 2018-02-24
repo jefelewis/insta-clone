7938b22bf3d0b514f932129db365e7f85d9cf418
Jake <jmleavell90@gmail.com>
2018-02-23 16:11:32 -0800

    rebase from Daniel please dont break stuff again

--- server/app.js
+++ server/app.js
@@ -9,21 +9,15 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../client/public/')));
 app.use('/', router);
 
-<<<<<<< 2d649c7bdbd49bbfc7eec7d6596e9e32ef461daa
-=======
+
 // Use: Static Files
-<<<<<<< f6d86130a071d563f6ee7a725e79d9fe8428d41b
 app.use(express.static(path.join(__dirname, '../client/public/')));
-=======
-app.use('/', express.static(path.join(__dirname, '../client/public')));
->>>>>>> Working Log In, No Dynamic Render
 
 // Use: Router
 app.use(router);
 
 
 // Listener
->>>>>>> Working Log In, No Dynamic Render
 const port = 9001;
 
 app.listen(port, function() {
