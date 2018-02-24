f2bb1b33ae73c8c70b54a7a6c40cd3c9eb56bd1e
jefelewis <jeffylew77@gmail.com>
2018-02-24 15:08:39 -0800

    Adding styling + follow button

--- client/public/components/views/profile/ProfileInfo.jsx
+++ client/public/components/views/profile/ProfileInfo.jsx
@@ -12,11 +12,30 @@ class ProfileInfo extends React.Component{
   render() {
     return (
       <div>
-        <h3>{this.props.username}</h3>
-        <h3>{this.props.bio}</h3>
-        <h3>Posts</h3>
-        <h3>Followers</h3>
-        <h3>Following</h3>
+        <div className="userdetails">
+          <h5><strong>user.username</strong></h5>
+          <h6>user.bio</h6>
+        </div>
+
+        <div className="posts">
+          <h6><strong>Posts</strong></h6>
+          <p>50</p>
+        </div>
+
+        <div className="followers">
+          <h6><strong>Followers</strong></h6>
+          <p>400</p>
+        </div>
+
+        <div className="following">
+          <h6><strong>Following</strong></h6>
+          <p>300</p>
+        </div>
+
+        <div className="followuser">
+          <button type="button" class="btn btn-primary">Follow</button>
+        </div>
+
       </div>
     );
   }
