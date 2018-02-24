4a3553b5819883cd1ffd2536fb2314ef497a5360
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-24 11:53:11 -0800

    lunch save

--- helpers/helperfunctions.js
+++ helpers/helperfunctions.js
@@ -1,7 +1,18 @@
 const functions = require('firebase-functions');
 const gcs = require('@google-cloud/storage')();
+const os = require('os');
+const cors = require('cors')({origin:true});
 const path = require('path');
 const Busboy = require('busboy');
+const fs = require('fs');
+
+exports.onFileChange = functions.storage.object().onChange(event => {
+  const object = event.data;
+  const bucket = object.bucket;
+  const contentType = object.contentType;
+  const filePath = object.name;
+  console.log('File change detected, starting function');
+})
 
 exports.uploadFile = functions.https.onRequest((req, res) =>{
   cors(req, res, () => {
@@ -16,6 +27,7 @@ exports.uploadFile = functions.https.onRequest((req, res) =>{
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
     const filepath = path.join(os.tmpdir(), filename);
     uploadData = {file: filepath, type:minetype}; 
+    file.pipe(fs.createWriteStream(filepath));
   });
   busboy.on('finish', () => {
     const bucket = gcs.bucket('gs://top-shelf-708be.appspot.com/') 
