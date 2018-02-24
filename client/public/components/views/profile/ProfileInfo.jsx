9ea80d6fa97d9355ef9a43c729cec153d944ec4b
jefelewis <jeffylew77@gmail.com>
2018-02-24 15:52:56 -0800

    Implementing bootstrap + styling changes

--- client/public/components/views/profile/ProfileInfo.jsx
+++ client/public/components/views/profile/ProfileInfo.jsx
@@ -11,32 +11,37 @@ class ProfileInfo extends React.Component{
 
   render() {
     return (
-      <div className="userinfo">
-        <div className="user">
-          <h5><strong>Jeff Lewis</strong></h5>
+      <div className="container col-md-8">
+
+        <div className="row">
+          <div className="col align-text-center">
+            <h5><strong>Jeff Lewis</strong></h5>
+          </div>
+
+          <div className="col">
+            <button type="button" className="btn btn-primary btn-sm"><a/>Follow</button>
+          </div>
         </div>
 
-        <div className="followuser">
-          <button type="button" className="btn btn-primary btn-sm">Follow</button>
+        <div className="row text-center">
+          <div className="col">
+            <h6><strong>Posts</strong></h6>
+            <p>50</p>
+          </div>
+          
+          <div className="col">
+            <h6><strong>Followers</strong></h6>
+            <p>400</p>
+          </div>
+          
+          <div className="col">
+            <h6><strong>Following</strong></h6>
+            <p>300</p>
+          </div>
         </div>
 
-        <div className="posts">
-          <h6><strong>Posts</strong></h6>
-          <p>50</p>
-        </div>
-        
-        <div className="followers">
-          <h6><strong>Followers</strong></h6>
-          <p>400</p>
-        </div>
-        
-        <div className="following">
-          <h6><strong>Following</strong></h6>
-          <p>300</p>
-        </div>
-
-        <div className="bio">
-          <h6>user.bio</h6>
+        <div className="row">
+          <h6 className="col text-center">This is a test bio</h6>
         </div>
         
       </div>
