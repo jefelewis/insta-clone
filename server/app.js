55ec0c6da482b7723d9147b6a9ccb6a8bce9df01
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-24 09:20:09 -0800

    quick save before rebase

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../client/public/')));
 app.use('/', router);
-
+// use /api/
 const port = 9001;
 
 app.listen(port, function() {
