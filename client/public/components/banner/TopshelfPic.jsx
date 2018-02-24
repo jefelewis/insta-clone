9978e3abce733525f9b8c928109baae64fea3b47
Daniel Khafi <Dkhaf001@gmail.com>
2018-02-23 16:10:12 -0800

    small change to pic

--- client/public/components/banner/TopshelfPic.jsx
+++ client/public/components/banner/TopshelfPic.jsx
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 
-
-
 class TopShelfPic extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div className="col-md-4">
-            <img src="../TopShelf.png" height="100" width="100" />
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="col-md-4">
+        <img src="../TopShelf.png" height="100" width="100" />
+      </div>
+    );
+  }
 }
 
 export default TopShelfPic;
